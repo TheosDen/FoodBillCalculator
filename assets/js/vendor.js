382c68023dd4e7d7b8e4c5534c1c60eedbdfb4e8
theos.den@gmail.com <ltybc3458>
2018-11-04 22:30:12 +0200

    Added Vendor file

---
+++ assets/js/vendor.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+
+import MaterialCore from '@material-ui/core';
+
+// import { withStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Badge from '@material-ui/core/Badge/Badge';
+// import List from '@material-ui/core/List/List';
+// import ListItem from '@material-ui/core/ListItem/ListItem';
+// import ListSubheader from '@material-ui/core/ListSubheader/ListSubheader';
+// import ListItemIcon from '@material-ui/core/ListItemIcon/ListItemIcon';
+//
+import DeleteIcon from '@material-ui/icons/Delete';
